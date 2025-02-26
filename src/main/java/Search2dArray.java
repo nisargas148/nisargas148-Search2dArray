9af4904a088de68236701874f0aae13fcf120504
nisargas148 <snisarga215@gmail.com>
2025-02-26 12:17:11 +0530

    done

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -9,7 +9,18 @@ public class Search2dArray {
      * @param target a value that may be contained within nums.
      * @return true if nums contains target, and false otherwise.
      */
-    public boolean search(int[][] nums, int target){
+    public boolean search(int[][] nums, int target) {
+        // Outer loop to iterate over rows
+        for (int i = 0; i < nums.length; i++) {
+            // Inner loop to iterate over columns in the current row
+            for (int j = 0; j < nums[i].length; j++) {
+                // If the target is found, return true
+                if (nums[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        // If target is not found after searching all rows and columns, return false
         return false;
     }
 }
